58d157a9b7839dd09cd7ca585ac7b5c891fbeb28
godo129 <godo129@naver.com>
2023-08-25 19:17:29 +0900

    feat: Then 과 같은 And Protocol 추가

---
+++ ToDoApplication/Projects/TodoApp/Sources/And/And.swift
@@ -0,0 +1,21 @@
+//
+//  And.swift
+//  ToDoApp
+//
+//  Created by hong on 2023/08/25.
+//  Copyright © 2023 co.godo. All rights reserved.
+//
+
+import Foundation
+
+public protocol And {}
+
+extension And where Self: AnyObject {
+    @inlinable
+    func and(_ adjust: ((Self) throws -> Void)) rethrows -> Self {
+        try adjust(self)
+        return self
+    }
+}
+
+extension NSObject: And {}
