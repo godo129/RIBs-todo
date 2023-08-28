a9f64315610d668f48c99650be5328ec24cddc73
godo129 <godo129@naver.com>
2023-08-28 15:45:14 +0900

    feat: 고양이 개 랜덤 사진 api response 모델 정의

---
+++ ToDoApplication/Projects/TodoApp/Sources/Model/DTO/Response/CatAndDogAPIResponse.swift
@@ -0,0 +1,18 @@
+//
+//  CatAndDogAPIResponse.swift
+//  ToDoApp
+//
+//  Created by hong on 2023/08/28.
+//  Copyright © 2023 co.godo. All rights reserved.
+//
+
+import Foundation
+
+struct CatAndDogAPIResponse: Codable {
+    
+    let id: String
+    let url: String
+    let width: Int
+    let height: Int
+    
+}
